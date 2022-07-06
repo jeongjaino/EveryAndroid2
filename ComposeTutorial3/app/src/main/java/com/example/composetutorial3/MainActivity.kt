36c348280952ed1c10d06253a52a0dc3e09c74ed
jaino <msb3302@naver.com>
2022-07-06 17:42:13 +0900

    android compose tutorial3 맞춤 레이아웃 전

--- ComposeTutorial3/app/src/main/java/com/example/composetutorial3/MainActivity.kt
+++ ComposeTutorial3/app/src/main/java/com/example/composetutorial3/MainActivity.kt
@@ -3,21 +3,26 @@ package com.example.composetutorial3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.example.composetutorial3.ui.theme.ComposeTutorial3Theme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,10 +65,90 @@ fun PhotographerCard(modifier : Modifier = Modifier){
     }
 }
 
+@Composable
+fun LayoutsCodelab(){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "LayoutsCodelab")
+                },
+                actions = {
+                    IconButton(
+                        onClick = {
+
+                        }
+                    ){
+                        Icon(Icons.Default.Favorite, contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) { innerPadding -> //scaffold의 람다는 padding을 매개변수로 가진다.
+        bodyContent(Modifier.padding(innerPadding))
+    }
+}
+@Composable
+fun bodyContent(modifier : Modifier = Modifier){
+    Column(modifier = modifier){
+        Text(text = "안녕하새우")
+        Text(text = "대하 타이거새우 흰다리새우 가재 랍스터")
+    }
+}
+
+@Composable
+fun ScrollingList(){
+
+    val listSize = 100
+
+    val scrollState = rememberLazyListState()
+    // 스크롤 포지션을 state로 저장
+
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(){
+        Row{
+            Button(onClick = {
+                coroutineScope.launch{
+                    scrollState.animateScrollToItem(0)
+                }
+            }){
+                Text("Scroll to the top")
+            }
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }){
+                Text("Scroll to the end")
+            }
+        }
+        LazyColumn(state = scrollState){
+            items(listSize){
+                ImageListItem(index = it)
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageListItem(index : Int){
+    Row(verticalAlignment = Alignment.CenterVertically){
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = null,
+            modifier= Modifier.size(50.dp)
+        )
+        Spacer(Modifier.width(10.dp))
+        Text(text = "Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
 @Preview
 @Composable
 fun PhotographerCardPreview() {
     ComposeTutorial3Theme {
-        PhotographerCard()
+        ScrollingList()
     }
 }
