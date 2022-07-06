fe9c7d66aaa77e42bde3d2cc30b403ad9bf3e1e8
jaino <msb3302@naver.com>
2022-07-07 01:16:45 +0900

    android compose tutorial3 constraint layout 전

--- ComposeTutorial3/app/src/main/java/com/example/composetutorial3/MainActivity.kt
+++ ComposeTutorial3/app/src/main/java/com/example/composetutorial3/MainActivity.kt
@@ -3,12 +3,12 @@ package com.example.composetutorial3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
@@ -17,8 +17,11 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.example.composetutorial3.ui.theme.ComposeTutorial3Theme
@@ -31,7 +34,7 @@ class MainActivity : ComponentActivity() {
             ComposeTutorial3Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    PhotographerCard()
+                    LayoutsCodelabs()
                 }
             }
         }
@@ -62,37 +65,6 @@ fun PhotographerCard(modifier : Modifier = Modifier){
                 Text("3 분전", style = MaterialTheme.typography.body2)
             }
         }
-    }
-}
-
-@Composable
-fun LayoutsCodelab(){
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = "LayoutsCodelab")
-                },
-                actions = {
-                    IconButton(
-                        onClick = {
-
-                        }
-                    ){
-                        Icon(Icons.Default.Favorite, contentDescription = null)
-                    }
-                }
-            )
-        }
-    ) { innerPadding -> //scaffold의 람다는 padding을 매개변수로 가진다.
-        bodyContent(Modifier.padding(innerPadding))
-    }
-}
-@Composable
-fun bodyContent(modifier : Modifier = Modifier){
-    Column(modifier = modifier){
-        Text(text = "안녕하새우")
-        Text(text = "대하 타이거새우 흰다리새우 가재 랍스터")
     }
 }
 
@@ -144,6 +116,127 @@ fun ImageListItem(index : Int){
         Spacer(Modifier.width(10.dp))
         Text(text = "Item #$index", style = MaterialTheme.typography.subtitle1)
     }
+}
+
+@Composable
+fun StaggeredGrid(
+    modifier : Modifier = Modifier,
+    rows : Int = 3,
+    content : @Composable () -> Unit
+){
+    Layout(
+        modifier = modifier,
+        content = content
+    ){ measurables, constraints ->
+
+        //행에 대한 너비 측정
+        val rowWidths = IntArray(rows) { 0 }
+
+        // 행에 대한 높이 측정
+        val rowHeights = IntArray(rows) { 0 }
+
+        // 하위 view들을 제한하지 않고, 주어진 제약조건들과 함께 측정한다.
+        val placeables = measurables.mapIndexed{ index, measurable ->
+
+            //하위요소 체크
+            val placeable = measurable.measure(constraints)
+
+            val row = index % rows
+            rowWidths[row] = placeable.width
+            rowHeights[row] = Math.max(rowHeights[row], placeable.height)
+
+            placeable
+        }
+
+        // Grid의 너비는 가장 넓은 행이다. coerceIn : 범위를 한정하는 함수
+        val width = rowWidths.maxOrNull()
+            ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
+
+        // Grid의 높이는 높이 제약조건으로 인해 강제로 변한된 각 행의 가장 높은 요소의 합이다.
+        val height = rowHeights.sumOf{it}
+            .coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        val rowY = IntArray(rows) { 0 }
+        for (i in 1 until rows){
+            rowY[i] = rowY[i-1] + rowHeights[i-1]
+        }
+        layout(width, height) {
+            // x cord we have placed up to, per row
+            val rowX = IntArray(rows) { 0 }
+
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+                rowX[row] += placeable.width
+            }
+        }
+    }
+}
+
+@Composable
+fun Chip(modifier: Modifier = Modifier, text: String) {
+    Card(
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(16.dp, 16.dp)
+                    .background(color = MaterialTheme.colors.secondary)
+            )
+            Spacer(Modifier.width(4.dp))
+            Text(text = text)
+        }
+    }
+}
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
+
+@Composable
+fun LayoutsCodelabs() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "LayoutsCodelab")
+                },
+                actions = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Row(modifier = modifier
+        .background(color = Color.LightGray)
+        .padding(16.dp)
+        .size(200.dp)
+        .horizontalScroll(rememberScrollState()),
+        content = {
+            StaggeredGrid {
+                for (topic in topics) {
+                    Chip(modifier = Modifier.padding(8.dp), text = topic)
+                }
+            }
+        })
 }
 @Preview
 @Composable
