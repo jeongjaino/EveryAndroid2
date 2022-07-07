5baa4791ee9fdbb8e23076dce66e0a77df97e2fb
jaino <msb3302@naver.com>
2022-07-07 23:47:20 +0900

    음악 플레이어 ui 작성

---
+++ Melon/app/src/main/java/com/example/melon/PlayerFragment.kt
@@ -0,0 +1,24 @@
+package com.example.melon
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PlayerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_player, container, false)
+    }
+
+    companion object{
+        fun newInstance(): PlayerFragment{
+            return PlayerFragment()
+        }
+    }
+}
