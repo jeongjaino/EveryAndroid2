55995edb0836a14e7f3a752d1eedfb7e04d3ea9f
jaino <msb3302@naver.com>
2022-07-10 21:05:08 +0900

    initial Commit

--- Github/app/src/main/java/com/example/github/MainActivity.kt
+++ Github/app/src/main/java/com/example/github/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.github
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.github.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy{ ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
+
     }
+
+    private fun initViews() = with(binding){
+        loginButton.setOnClickListener {
+            loginGithub()
+        }
+    }
+
+    private fun loginGithub(){
+        val loginUri = Uri.Builder().scheme("https").authority("github.com")
+            .appendPath("login")
+            .appendPath("oauth")
+            .appendPath("authorize")
+            .appendQueryParameter("client_id", BuildConfig.GITHUB_CLIENT_ID)
+            .build()
+    }
+
 }
