cb2c0d718e7ab4a88d2b16499659d1afa78b7abf
jaino <msb3302@naver.com>
2023-02-04 00:35:12 +0900

    E2E Test를 위한 Test Tag 수정하기

--- AndroidTestExample/app/src/main/java/com/plcoding/cleanarchitecturenoteapp/core/util/TestTags.kt
+++ AndroidTestExample/app/src/main/java/com/plcoding/cleanarchitecturenoteapp/core/util/TestTags.kt
@@ -1,12 +1,14 @@
 package com.plcoding.cleanarchitecturenoteapp.core.util
 
 object TestTags {
-
     const val ORDER_SECTION = "ORDER_SECTION"
-
     const val TITLE = "Title"
     const val DATE = "Date"
     const val COLOR = "Color"
     const val ASCENDING = "Ascending"
     const val DESCENDING = "Descending"
+
+    //E2E
+    const val TITLE_TEXT_FIELD = "TITLE_TEXT_FIELD"
+    const val CONTENT_TEXT_FIELD = "CONTENT_TEXT_FIELD"
 }
