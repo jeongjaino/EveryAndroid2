1fe1fc4a1bba52d8331b42b9f04506093b825004
jaino <msb3302@naver.com>
2023-02-04 00:43:29 +0900

    E2E Test 노트 작성하고, 수정하는 작업 테스트 코드 작성

---
+++ AndroidTestExample/app/src/androidTest/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/NotesEndToEndTest.kt
@@ -0,0 +1,123 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.plcoding.cleanarchitecturenoteapp.core.util.TestTags
+import com.plcoding.cleanarchitecturenoteapp.di.AppModule
+import com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.add_edit_note.AddEditNoteScreen
+import com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes.NotesScreen
+import com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.util.Screen
+import com.plcoding.cleanarchitecturenoteapp.ui.theme.CleanArchitectureNoteAppTheme
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@HiltAndroidTest
+@UninstallModules(AppModule::class)
+class NotesEndToEndTest {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeRule = createAndroidComposeRule<MainActivity>()
+
+    @OptIn(ExperimentalAnimationApi::class)
+    @Before
+    fun setUp(){
+        hiltRule.inject()
+        composeRule.setContent {
+            CleanArchitectureNoteAppTheme {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Screen.NotesScreen.route
+                ) {
+                    composable(route = Screen.NotesScreen.route) {
+                        NotesScreen(navController = navController)
+                    }
+                    composable(
+                        route = Screen.AddEditNoteScreen.route +
+                                "?noteId={noteId}&noteColor={noteColor}",
+                        arguments = listOf(
+                            navArgument(
+                                name = "noteId"
+                            ) {
+                                type = NavType.IntType
+                                defaultValue = -1
+                            },
+                            navArgument(
+                                name = "noteColor"
+                            ) {
+                                type = NavType.IntType
+                                defaultValue = -1
+                            },
+                        )
+                    ) {
+                        val color = it.arguments?.getInt("noteColor") ?: -1
+                        AddEditNoteScreen(
+                            navController = navController,
+                            noteColor = color
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun saveNewNote_editAfterwards(){
+        // 노트 추가 FAB 클릭
+        composeRule.onNodeWithContentDescription("Add").performClick()
+
+        // 임의의 내용과 제목 입력
+        composeRule
+            .onNodeWithTag(TestTags.TITLE_TEXT_FIELD)
+            .performTextInput("test-title")
+        composeRule
+            .onNodeWithTag(TestTags.CONTENT_TEXT_FIELD)
+            .performTextInput("test-content")
+        // 저장하기
+        composeRule.onNodeWithContentDescription("Save").performClick()
+
+        // 작성한 제목과 내용이 들어간 노트가 리스트에 있는 지 확인
+        composeRule.onNodeWithText("test-title").assertIsDisplayed()
+        // 리스트에 저장한 노트 클릭하여 수정 화면으로
+        composeRule.onNodeWithText("test-content").performClick()
+
+        // Text Field 에 제대로 작성한 내용이 있는지 확인
+        composeRule
+            .onNodeWithTag(TestTags.TITLE_TEXT_FIELD)
+            .assertTextEquals("test-title")
+        composeRule
+            .onNodeWithTag(TestTags.CONTENT_TEXT_FIELD)
+            .assertTextEquals("test-content")
+        // 제목에 "2" 내용 추가하기
+        composeRule
+            .onNodeWithTag(TestTags.TITLE_TEXT_FIELD)
+            .performTextInput("2")
+
+        // 수정한 내용 저장하기
+        composeRule.onNodeWithContentDescription("Save").performClick()
+
+        // 수정한 내용 제대로 들어갔는지 확인
+        composeRule.onNodeWithText("test-title2").assertIsDisplayed()
+    }
+
+    @Test
+    fun saveNewNotes_orderByTitleDescending(){
+        repeat(3){
+            
+        }
+    }
+
+}
