82ebd04431d1935fc103686a1db75dd44f44b466
jaino <msb3302@naver.com>
2023-02-03 14:51:51 +0900

    통합 테스트를 위한 TestTags 생성

---
+++ AndroidTestExample/app/src/main/java/com/plcoding/cleanarchitecturenoteapp/core/util/TestTags.kt
@@ -0,0 +1,12 @@
+package com.plcoding.cleanarchitecturenoteapp.core.util
+
+object TestTags {
+
+    const val ORDER_SECTION = "ORDER_SECTION"
+
+    const val TITLE = "Title"
+    const val DATE = "Date"
+    const val COLOR = "Color"
+    const val ASCENDING = "Ascending"
+    const val DESCENDING = "Descending"
+}
