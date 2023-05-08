1305b2c7200c5b8bd715f709a773889f68812ea8
jeongjaino <akgk155@gmail.com>
2023-05-08 11:05:20 +0900

    (CachingPaging) initial commit

---
+++ CachingPaging/app/src/main/java/com/jaino/cachingpaging/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.jaino.cachingpaging
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jaino.cachingpaging.ui.theme.CachingPagingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CachingPagingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CachingPagingTheme {
+        Greeting("Android")
+    }
+}
